b67fc84cc79425278d1c098a164945e298d99616
devpnThunder <devpthunder@gmail.com>
2024-01-05 14:03:47 +0300

    Task #2 commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name_- == "__main__":
+	"""prints the number of a list of arguments."""
+	import os
+
+	count = len(sys.argv0 - 1
+	if count == 0:
+		print("0 arguments.")
+	elif count == 1:
+		print("1 argument:")
+	else:
+		print("{} arguments:".format(count))
+	for i in range(count):
+		print("{}: {}".format(i + 1, sys.argv[i + 1]))
