7d93a8544ac192ac42a40adf54979829e795f4fa
devpnThunder <devpthunder@gmail.com>
2024-03-14 03:57:57 +0300

    Task #12 #2

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- A script that updates the score of Bob to 10 in a table
-UPDATE INTO `second_table` (`score`) VALUES (10) WHERE `name` = "Bob";
+UPDATE `second_table` SET `score` = 10 WHERE `name` = "Bob";
