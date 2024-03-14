faab095c61059bda811546301006aa8466abbb48
devpnThunder <devpthunder@gmail.com>
2024-03-14 04:15:18 +0300

    Task #15 commit #3

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,7 @@
--- A script that lists the number of records with the same score in the table of a database in MySQL server
-SELECT `score`, COUNT(`*`) AS `number` 
-FROM `second_table` 
+-- A script that lists the number of records with the same score
+-- The result should display the score
+-- The result should display the number of records for this score with the label number
+SELECT `score`, COUNT(*) AS `number` 
+FROM `second_table`
 GROUP BY `score`
 ORDER BY `number` DESC;
