0147f922eca66c05badce513189ecb97b96ebeee
devpnThunder <devpthunder@gmail.com>
2024-04-11 00:46:40 +0300

    Task #3 commit #2

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv[2] !== NULL){
-console.log(process.argv[2]);
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-console.log("No argument");
+  console.log(process.argv[2]);
 }
