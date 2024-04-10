4ff2ba854bf946b2035b8db24d43ed5bd96d3ec2
devpnThunder <devpthunder@gmail.com>
2024-04-11 00:32:26 +0300

    Task #2 commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if(argv === 0){
+console.log("No argument");
+} elseif (argv === 1){
+console.log("Argument found");
+} else {
+console.log("Arguments found");
+}
