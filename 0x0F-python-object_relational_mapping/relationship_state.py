f608201230f5a71e54053b505f4766cd465c9b0a
devpnThunder <devpthunder@gmail.com>
2024-04-18 16:07:54 +0300

    Advanced Task #101 Realtionship State and City commit #4

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 """
 Defines a state model that contain the class definition
- of a City and an instance Base = declarative_base()
+ of a State and an instance Base = declarative_base()
 """
 from lib2to3.pytree import Base
 from sre_parse import State
 from unicodedata import name
-from sqlalchemy import Column, ForeignKey, Integer, String, null
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-
+from relationship_city import Base, City
 Base = declarative_base()
 
 
-class City(Base):
+class State(Base):
     """
-    inherits from Base (imported from model_state)
-    links to the MySQL table cities
-
-    class attribute id that represents a column of
-    an auto-generated, unique integer, can't be null and is a primary key
-
+    inherits from Base Tips
+    links to the MySQL table states
+    class attribute id that represents a column
+     of an auto-generated, unique integer, can't
+      be null and is a primary key
     class attribute name that represents a column
-    of a string of 128 characters and can't be null
-
-    class attribute state_id that represents a column
-    of an integer, can't be null and is a foreign key to states.id
+     of a string with maximum 128 characters and
+      can't be null
 
     """
-    __tablename__ = "cities"
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
